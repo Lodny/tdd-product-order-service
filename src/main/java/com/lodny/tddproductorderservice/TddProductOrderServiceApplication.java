f27832dd503601f33dd854d37559ff7125361fd6
Lodny <drinkjuice@naver.com>
2023-02-18 21:15:52 +0900

    init

---
+++ src/main/java/com/lodny/tddproductorderservice/TddProductOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lodny.tddproductorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddProductOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddProductOrderServiceApplication.class, args);
+    }
+
+}
